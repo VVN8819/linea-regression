0dcaff5d72d97f2b0388e2a585ef7a9527883b45
VVN8819 <vitalii8819@gmail.com>
2026-06-20 17:47:20 +0800

    features: README дополнение Корреляционный анализ

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -5,10 +5,12 @@ from .data_loader import read_csv_data
 from .eda import run_eda
 from .visualization import run_visualizations
 from .feature_engineering import run_feature_engineering
+from .correlations import run_correlations
 
 __all__ = [
     'read_csv_data', 
     'run_eda', 
     'run_visualizations',
-    'run_feature_engineering'
+    'run_feature_engineering',
+    'run_correlations'
 ]
